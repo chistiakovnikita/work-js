ede4ddb628ab0d362ae932dfc3d138ece8c4d57a
Nikita <nikita_chnv@mail.ru>
2022-09-23 21:37:18 +0300

    object function

--- src/components/button/button.js
+++
@@ -1,5 +0,0 @@
-import './button.scss';
-
-export const Button = () => {
-    return `<button class="button">Click</button>`
-}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
 import "./main.scss";
-import { Button } from './components/button/button';
 
-const add123 = (a, b) => a + b;
-const root = document.querySelector('#root');
 
-root.insertAdjacentHTML('beforeend', Button())
 
-const res = add123(222, 3);
-console.log(root);
+
+
+
+
+// const currency = {
+//     usd: 'dollar',
+//     blr: 'bel rouble',
+//     rus: 'russian rouble'
+// }
+
+
+
+// let descriptor = Object.defineProperty(currency, 'usd',{
+// writable:false,
+// });
+
+// currency.usd = 'aghjhdashjds'
+// alert(descriptor[currency]);
+
+
+
+// function sum (a,b){
+//     alert( a +b)
+// }
+
+// sum( 1,2)
+
+
+// let sum
+// let sum1
+// const fn = (...param) =>{
+//     let sum = 0;
+//     for ( sum1 of param) {
+//         sum += sum1
+//     }
+
+//     return sum;
+// }
+
+// console.log(fn(1,2,3,4,5));
+
+let min = (a,b) => {
+if (a<b){
+    return a;
+}else{
+        return b
+    }
+}
+
+console.log(min(50,10));
