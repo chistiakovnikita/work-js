033bc1bfeb2735aac23a82eda784506b45b76736
Nikita <nikita_chnv@mail.ru>
2022-09-30 21:22:57 +0300

    massiv

--- src/index.js
+++ src/index.js
@@ -43,12 +43,69 @@ import "./main.scss";
 
 // console.log(fn(1,2,3,4,5));
 
-let min = (a,b) => {
-if (a<b){
-    return a;
-}else{
-        return b
-    }
-}
+// let min = (a,b) => {
+// if (a<b){
+//     return a;
+// }else{
+//         return b
+//     }
+// }
 
-console.log(min(50,10));
+// console.log(min(50,10));
+
+
+// const sumTo =(n) => {
+//     if (n===1) return 1;
+//     return n +sumTo(n-1);
+    
+// }
+
+// consol.log(sumTo(10));
+
+
+// function sum (a) {
+//     return function (b) {
+//         return a+b;
+//     };
+// }
+
+
+// const list ={
+//     a: 'a',
+//     b: 'b',
+//     next: {
+//         a: 'a-1',
+//         b: 'a-1',
+//         next: {
+//            a: 'a-2',
+//            b: 'a-2',
+//             next:null
+           
+//         }
+//     } 
+// };
+
+// const printA =(list) => {
+//     list.a =Math.rundom();
+//     if (list.next)
+
+    
+// }
+
+
+
+
+// let string = 'aaa bbb ccc';
+// console.log(string.substring(4,8));
+// console.log(string.slice(4,7));
+// console.log(string.substr(3));
+
+let array = [ 1, 2, 3, 'underfind', 0, null, '', 'false', 5, 6];
+
+
+let compact = array.filter((array) => {
+    array.length === 'underfind' || 'null' || 0 || '', 'false';
+    return array;
+}
+)
+console.log(compact);
